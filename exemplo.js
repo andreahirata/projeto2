8c8c85b418fe0f6ca5fb71cce85e57777e770c5d
Andrea Hirata <de.hirata@gmail.com>
2018-01-23 22:45:42 -0200

    primeiro commit

---
+++ exemplo.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector('button')
+let titulo = document.querySelector('h2');
+
+function MudarCorh1() {
+  titulo.style.color="red";
+  return false;
+}
---
+++ script.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector('h1');
+let texto = document.querySelector('p');
+
+function mudarTexto() {
+  titulo.style.color = "white";
+}
